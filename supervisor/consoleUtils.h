f774b4a0346056e67943bd500a39b0f8c2c1f8c1
superstraz <super.stas.stanislaw@gmail.com>
2018-10-11 01:58:22 +0300

    add 4.1 and 4.2

---
+++ supervisor/consoleUtils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <assert.h>
+
+int readNumber(int argc, char *argv[]);
+
--- supervisor/first_task.c
+++ supervisor/first_task.c
@@ -1,45 +1,10 @@
 #include <stdio.h>
 #include <errno.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <assert.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-int readNumber(int argc, char *argv[])
-{
-    assert(argv);
-    if (argc < 2)
-    {
-        printf("error, expected 1 number\n");
-        exit(EXIT_FAILURE);
-    }
+#include "consoleUtils.h"
 
-    char *str = argv[1];
-    char *endPtr;
-
-    errno = 0;
-    int val = (int) strtol(str, &endPtr, 10);
-
-    if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
-        || (errno != 0 && val == 0))
-    {
-        printf("error, can't read number\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (endPtr == str)
-    {
-        printf("No digits were found\n");
-        return -1;
-    }
-
-    if (*endPtr != '\0')
-        printf("Further characters after number: %s\n", endPtr);
-
-    return val;
-
-}
 
 int main(int argc, char *argv[])
 {
